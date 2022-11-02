787581ca6df9cc2508ac580ba1143db81aa44af1
codewithjosh <dacasinjoshua21@gmail.com>
2022-11-02 16:26:31 +0800

    refactor: finishing touches

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,30 @@ import 'package:flutter/services.dart';
 import 'package:news_expose_2k21/main_screen.dart';
 import 'package:news_expose_2k21/functions.dart';
 
-void main() async
-{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp],
   );
-  runApp(
-      const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Main(),
-      )
-  );
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Main(),
+  ));
 }
 
-class Main extends StatefulWidget
-{
+class Main extends StatefulWidget {
   const Main({Key? key}) : super(key: key);
   @override
   _MainState createState() => _MainState();
 }
 
-class _MainState extends State<Main>
-{
+class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) => const AnnotatedRegion(
-    value: SystemUiOverlayStyle(
-      statusBarColor: colorChineseBlack,
-      statusBarIconBrightness: Brightness.light,
-    ),
-    child: MainScreen(),
-  );
+        value: SystemUiOverlayStyle(
+          statusBarColor: colorChineseBlack,
+          statusBarIconBrightness: Brightness.light,
+        ),
+        child: MainScreen(),
+      );
 }
