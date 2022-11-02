a130d9de1f1bb3d74d3857007f3a241919e5501d
codewithjosh <dacasinjoshua21@gmail.com>
2022-11-02 16:14:13 +0800

    feat: add `login` and `register` nav button

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_expose_2k21/functions.dart';
+import 'package:news_expose_2k21/login_screen.dart';
+import 'package:news_expose_2k21/register_screen.dart';
 
 class MainScreen extends StatelessWidget
 {
@@ -15,36 +17,42 @@ class MainScreen extends StatelessWidget
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    height: 50.0,
-                    decoration: const BoxDecoration(
-                      gradient: linearButton,
-                    ),
-                    child: const Center(
-                      child: Text(
+                  child: GestureDetector(
+                    onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const LoginScreen())),
+                    child: Container(
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        gradient: linearButton,
+                      ),
+                      child: const Center(
+                        child: Text(
                         'LOGIN',
-                        style: TextStyle(
-                          fontFamily: 'Calibri',
-                          fontSize: 25,
-                          color: Colors.white,
+                          style: TextStyle(
+                            fontFamily: 'Calibri',
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    height: 50.0,
-                    decoration: const BoxDecoration(
-                      color: colorChineseBlack,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'REGISTER',
-                        style: TextStyle(
-                          fontFamily: 'Calibri',
-                          fontSize: 25,
-                          color: Colors.white,
+                  child: GestureDetector(
+                    onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const RegisterScreen())),
+                    child: Container(
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        color: colorChineseBlack,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'REGISTER',
+                          style: TextStyle(
+                            fontFamily: 'Calibri',
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
