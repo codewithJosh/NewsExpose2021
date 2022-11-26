06bc8225c513f9ee91550e1263980d86d3b6668c
codewithJosh <dacasinjoshua21@gmail.com>
2022-11-26 08:10:03 +0800

    feat: add `Comment` layout

--- lib/comment_screen.dart
+++ lib/comment_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:news_expose_2k21/functions.dart';
 
 class CommentScreen extends StatefulWidget {
   final String updateId;
@@ -10,8 +12,41 @@ class CommentScreen extends StatefulWidget {
 }
 
 class _CommentScreenState extends State<CommentScreen> {
+
+  _initAppBar() => AppBar(
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    backgroundColor: colorChineseBlack,
+    toolbarHeight: 55.0,
+
+    flexibleSpace: SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: linearAppBar,
+        ),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          margin: const EdgeInsets.only(left: 60.0),
+          child: initTitle2('Comments', size: 23.0),
+        ),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Scaffold(
+
+      appBar: _initAppBar(),
+
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('res/images/img_background_2.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }
